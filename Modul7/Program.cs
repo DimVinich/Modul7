e2cc8b80ea0aaf9c1dd23e26851bc48b448d8676
DimVinich <DmitrijVS@gmail.com>
2022-06-07 19:17:38 +0300

    Задание 7.1.4

--- Modul7/Program.cs
+++ Modul7/Program.cs
@@ -6,7 +6,65 @@ namespace Modul7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
+    }
+
+	class ProjectManager : Employee
+    {
+		public string ProjectName;
+
+	}
+
+	class Developer : Employee
+    {
+		public string ProgrammingLanguage;
+
+	}
+
+	class Employee
+	{
+		public string Name;
+		public int Age;
+		public int Salary;
+	}
+
+	class HybridCar : Car
+	{
+		public FuelType FuelType;
+
+		public void ChangeFuelType(FuelType type)
+		{
+			FuelType = type;
+		}
+	}
+	class Car
+	{
+		public double Fuel;
+		public int Mileage;
+
+		public Car()
+		{
+			Fuel = 50;
+			Mileage = 0;
+		}
+
+		public void Move()
+		{
+			// Move a kilometer
+			Mileage++;
+			Fuel -= 0.5;
+		}
+
+		public void FillTheCar()
+		{
+			Fuel = 50;
+		}
+	}
+	enum FuelType
+    {
+        Gas = 0,
+        Electricity
     }
 }
