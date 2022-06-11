e9b261960698784fc974b6520a9a4090d6b26487
DimVinich <DmitrijVS@gmail.com>
2022-06-11 07:29:31 +0300

    Задание 7.1.7

--- Modul7/Program.cs
+++ Modul7/Program.cs
@@ -6,10 +6,29 @@ namespace Modul7
     {
         static void Main(string[] args)
         {
+			SmartHelper helper = new SmartHelper("Олег");
+			helper.Greetings("Грег");
 
             Console.ReadLine();
         }
     }
+
+
+
+	class SmartHelper 
+	{
+	  private string name;
+
+	  public SmartHelper(string name) 
+	  {
+		this.name = name;
+	  }
+
+	  public void Greetings(string name) 
+	  {
+		Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+	  }
+	}
 
 	class Herb { }
 	class Fruit : Herb { }
