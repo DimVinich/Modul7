eac4077f8793d9a095b26c5b85dd451eb1591465
DimVinich <DmitrijVS@gmail.com>
2022-06-13 18:22:54 +0300

    Задание 7.6.2

--- Modul7/Program.cs
+++ Modul7/Program.cs
@@ -13,22 +13,76 @@ namespace Modul7
 		}
 	}
 
+	abstract class Transport
+	{
+		public abstract void Move();
+	}
+	class Boat : Transport
+	{
+		public override void Move()
+		{
+			// ...
+		}
+	}
+	class HybridCar : Car
+	{
+		public FuelType FuelType;
+
+		public void ChangeFuelType(FuelType type)
+		{
+			FuelType = type;
+		}
+	}
+	
+	class ElectricEngine { }
+	class GasEngine { }
+
+	class Car<T> : Transport 
+	{
+		public double Fuel;
+		public int Mileage;
+		public T Engine { get; set; }
+
+		public Car()
+		{
+			Fuel = 50;
+			Mileage = 0;
+		}
+
+		public override void Move()
+		{
+			Mileage++;
+			Fuel -= 0.5;
+		}
+
+		public void FillTheCar()
+		{
+			Fuel = 50;
+		}
+	}
+	enum FuelType
+	{
+		Gas = 0,
+		Electricity
+	}
+
+
 	static class IntEctensions
-    {
+	{
 		public static int GetNegative(this int ai)
-        {
-			if(ai > 0)
-            {
+		{
+			if (ai > 0)
+			{
 				return ai * (-1);
-            }
-            else
-            {
+			}
+			else
+			{
 				return ai;
-            }
-        }
+			}
+		}
 
 		public static int GetPosotove(this int ai)
-        {
+		{
 			if (ai < 0)
 			{
 				return ai * (-1);
@@ -40,7 +94,6 @@ namespace Modul7
 
 		}
 	}
-
 	static class Helper
 	{
 		static void Swap(ref int aa, ref int ab)
@@ -126,41 +179,4 @@ namespace Modul7
 		public int Salary;
 	}
 
-	class HybridCar : Car
-	{
-		public FuelType FuelType;
-
-		public void ChangeFuelType(FuelType type)
-		{
-			FuelType = type;
-		}
-	}
-	class Car
-	{
-		public double Fuel;
-		public int Mileage;
-
-		public Car()
-		{
-			Fuel = 50;
-			Mileage = 0;
-		}
-
-		public void Move()
-		{
-			// Move a kilometer
-			Mileage++;
-			Fuel -= 0.5;
-		}
-
-		public void FillTheCar()
-		{
-			Fuel = 50;
-		}
-	}
-	enum FuelType
-	{
-		Gas = 0,
-		Electricity
-	}
 }
